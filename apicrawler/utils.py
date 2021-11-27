e25e2881fd33ce67ee870c01da97ce05096497fd
Fabio402 <59377339+Fabio402@users.noreply.github.com>
2021-11-27 14:58:27 -0300

    Finalização do projeto
    Finalização das atividades relacionadas ao crawler

--- apicrawler/utils.py
+++ apicrawler/utils.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.service import Service
-
+import time
 
 class Crawler:
     """
@@ -22,11 +22,11 @@ class Crawler:
     def __init__(self) -> None:
         self.URL = 'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops'
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
         
-        #service = Service('../chromedriver.exe')
+        #service = Service('chromedriver.exe')
 
-        self.chrome = webdriver.Chrome(chrome_options=options)
+        self.chrome = webdriver.Chrome(options=options)
 
     def searching_notebooks(self) -> dict:
         """
@@ -39,15 +39,29 @@ class Crawler:
         self.chrome.get(self.URL)
 
         thumbnails = self.chrome.find_elements(By.CLASS_NAME, 'thumbnail')
-        for thumbnail in thumbnails:
-            img = thumbnail.find_element(By.CLASS_NAME, 'img-responsive').get_attribute('src')
-            price = thumbnail.find_element(By.CLASS_NAME, 'price').text
-            title = thumbnail.find_element(By.CLASS_NAME, 'title').text.lower()
-            description = thumbnail.find_element(By.CLASS_NAME, 'description').text
-            reviews = thumbnail.find_element(By.CLASS_NAME, 'ratings').find_element(By.CLASS_NAME, 'pull-right').text.replace(' reviews', '')
-            stars = thumbnail.find_element(By.CLASS_NAME, 'ratings').find_element(By.XPATH, 'p[@data-rating]').get_attribute('data-rating')
+        urls = [(thumbnail.find_element(By.CLASS_NAME, 'title').get_attribute('href')) for thumbnail in thumbnails]        
+
+        for url in urls:
+            self.chrome.get(url)
+            time.sleep(2)
+            self.chrome.find_element_by_id('closeCookieBanner').click()
+            img = self.chrome.find_element(By.CLASS_NAME, 'img-responsive').get_attribute('src')
+            h4 = self.chrome.find_element(By.CLASS_NAME, 'caption').find_elements(By.TAG_NAME, 'h4')
+            price = h4[0].text.replace('$','')
+            title = h4[1].text.lower()
+            description = self.chrome.find_element(By.CLASS_NAME, 'description').text
+            reviews = self.chrome.find_element(By.CLASS_NAME, 'ratings').find_element(By.TAG_NAME, 'p').text.replace(' reviews', '')
+            stars = len(self.chrome.find_elements(By.CLASS_NAME, 'glyphicon-star'))
+            hds = self.chrome.find_elements_by_class_name('swatch')
+            prices = {}
+            for hd in hds:
+                    if not 'disabled' in hd.get_attribute('class'):
+                        hd.click()                                      
+                        prices[hd.text] = self.chrome.find_element(By.CLASS_NAME, 'price').text
+
             yield {'img': img,
-                   'price': price,
+                   'price': float(price),
+                   'prices': prices,
                    'title': title,
                    'description': description,
                    'reviews': reviews,
